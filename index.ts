1ccc4eb8aa09e4ca3240cced29b2b9396af46c8a
Ondřej Žára <ondrej.zara@gmail.com>
2024-05-06 13:22:21 +0200

    cron

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.192.0/http/server.ts"
-import { serveFile } from "https://deno.land/std@0.192.0/http/file_server.ts"
-import { posix, join } from "https://deno.land/std@0.192.0/path/mod.ts"
+import { serveFile } from "https://deno.land/std@0.224.0/http/file_server.ts"
+import { join } from "https://deno.land/std@0.224.0/path/mod.ts"
+import { normalize } from "https://deno.land/std@0.224.0/path/posix/mod.ts"
 
 let clients = new Set<WebSocket>()
 let data = new Map<string, string>()
@@ -40,13 +40,12 @@ function onClientOpen(socket: WebSocket) {
 	}
 }
 
-async function reset(url: URL) {
-	for (let [skey, value] of data) {
+async function reset() {
+	for (let skey of data.keys()) {
 		let key = skey.split(",").map(Number)
 		await kv.delete(key)
 	}
 	data.clear()
-	return Response.redirect(new URL("/", url))
 }
 
 async function handler(request: Request) {
@@ -60,16 +59,30 @@ async function handler(request: Request) {
 	} else {
 		const url = new URL(request.url)
 		let path = decodeURIComponent(url.pathname)
-		if (path == "/reset") { return reset(url) }
+		if (path == "/reset") {
+			await reset()
+			return Response.redirect(new URL("/", url))
+		}
 		if (path == "/") { path = "/index.html" }
-		path = join("./static", posix.normalize(path))
+		path = join("./static", normalize(path))
 		return serveFile(request, path)
 	}
 }
 
 let initialData = kv.list<string>({prefix:[]})
 for await (let item of initialData) {
+	console.log("initial data", item.key, item.value);
 	data.set(item.key.join(","), item.value)
 }
 
-serve(handler)
+Deno.cron("reset", "0 */2 * * *", reset)
+
+Deno.serve(handler)
+/*
+let stream = kv.watch([["asd"]]);
+console.log("watch stream", stream);
+for await (let change of stream) {
+	console.log(change);
+}
+console.log("watch done");
+*/
